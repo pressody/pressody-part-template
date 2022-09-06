9595c4c5854912caed970c539ec45b92c04057ec
Vlad Olaru <vlad@pixelgrade.com>
2022-09-06 20:27:55 +0300

    Change author email

--- pressody-part-template.php
+++ pressody-part-template.php
@@ -3,7 +3,7 @@
  * Pressody Part Template Title
  *
  * @package Pressody
- * @author  Vlad Olaru <vladpotter85@gmail.com>
+ * @author  Vlad Olaru <vlad@thinkwritecode.com>
  * @license GPL-2.0-or-later
  *
  * @wordpress-plugin
